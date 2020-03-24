c218627d6404355bdfa892507ad6619c47098a55
Mamednoor <mamed.gohabur@gmail.com>
2020-03-24 12:34:59 +0100

    clean

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -5,13 +5,12 @@ const fizzBuzz = (number) => {
 } 
   else if (number % 3 === 0) {
     return "fizz"
-} 
+}
   else if (number % 5 === 0) {
     return "buzz"
 } 
   else {
     return number
-}
 };
-
+}
 module.exports = fizzBuzz;
