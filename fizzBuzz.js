009800cef045536b340b592221d0d7acac6a3457
Mamednoor <mamed.gohabur@gmail.com>
2020-03-24 12:25:39 +0100

    Resolving fizzbuzz Aristide, Mohammed, Mamed

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,5 +1,17 @@
-const fizzBuzz = () => {
-  // Start here
+const fizzBuzz = (number) => {
+    // Start here
+  if (number % 3 === 0 && number % 5 === 0) {
+    return "fizzbuzz"
+} 
+  else if (number % 3 === 0) {
+    return "fizz"
+} 
+  else if (number % 5 === 0) {
+    return "buzz"
+} 
+  else {
+    return number
+}
 };
 
 module.exports = fizzBuzz;
